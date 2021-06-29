6f0b1872a420f3357ec15517e4f0bf1572e2ca07
peace-among-worlds <34693070+peace-among-worlds@users.noreply.github.com>
2021-06-29 17:01:00 +1200

    Add initial files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navigation from './Navigation/Navigation';
+import Logo from './Logo/Logo';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
